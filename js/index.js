b7b5d0ec1e6b763932a8895c203cf4b8f7ca7039
bop001 <sashaburlachenko@gmail.com>
2018-12-18 08:22:09 +0200

    ready two section

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	// $( "#slider-range-min" ).slider({
+ //      range: "min",
+ //      value: 125,
+ //      min: 1,
+ //      max: 700,
+ //      slide: function(event,ui) {
+ //        $( "#amount" ).val( "м<sup><small>2</sup></small>" + ui.value );
+ //      }
+ //    });
+  /*$("input[type='range']").range({
+  fill: true
+  });*/
+});
