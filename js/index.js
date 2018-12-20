90542bcd54a8dd2a487c4d61902db6ee9b74ad36
bop001 <sashaburlachenko@gmail.com>
2018-12-20 14:01:19 +0200

    edit input in form

--- js/index.js
+++ js/index.js
@@ -1,14 +1,23 @@
 $(document).ready(function(){
-	// $( "#slider-range-min" ).slider({
- //      range: "min",
- //      value: 125,
- //      min: 1,
- //      max: 700,
- //      slide: function(event,ui) {
- //        $( "#amount" ).val( "м<sup><small>2</sup></small>" + ui.value );
- //      }
- //    });
-  /*$("input[type='range']").range({
-  fill: true
-  });*/
+	$( "#slider-range-min" ).slider({
+      range: "min",
+      value: 50,
+      min: 1,
+      max: 1700,
+      slide: function( event, ui ) {
+        $( "#amount" ).val( ui.value + "м2");
+      }
+    });
+    $( "#amount" ).val( $( "#slider-range-min" ).slider( "value" ) + "м2");
+    $( "#slider-range-min2" ).slider({
+      range: "min",
+      value: 50,
+      min: 1,
+      max: 1700,
+      slide: function( event, ui ) {
+        $( "#amount2" ).val( ui.value + "м2");
+      }
+    });
+    $( "#amount2" ).val( $( "#slider-range-min2" ).slider( "value" ) + "м2");
+    $( "#tabs" ).tabs();
 });
