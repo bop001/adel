2631d8af956f48a124971e2be08c84cbf65732da
bop001 <sashaburlachenko@gmail.com>
2018-12-21 14:45:40 +0200

    problems section portfolio

--- js/index.js
+++ js/index.js
@@ -20,9 +20,7 @@ $(document).ready(function(){
     });
     $( "#amount2" ).val( $( "#slider-range-min2" ).slider( "value" ) + "м2");
     
-
- initTabs("#tabs_1", "#tabs_2", "#tabs_3", "#tabs_4", "#tabs_5");
-
+  
 /*$(window).on("ionTabsChange", function(){
   $('.loft1-for').slick();
     
@@ -32,32 +30,78 @@ $(document).ready(function(){
     console.log(obj.tabId);
 });*/
 
-
-$('.loft1-for').slick({
+var propsFor = {
   slidesToShow: 1,
   slidesToScroll: 1,
   arrows: false,
   fade: true,
   lazyLoad: 'ondemand',
-  asNavFor: '.loft1-nav'
-});
-$('.loft1-nav').slick({
+  asNavFor: '.slider-nav'
+};
+var propsNav = {
   slidesToShow: 4,
   slidesToScroll: 1,
-  asNavFor: '.loft1-for',
+  asNavFor: '.slider-for',
   arrows: true,
   focusOnSelect: true,
   vertical: true,
   verticalSwiping: true
-});
+};
+
+initSlick($('.slider-for'), propsFor);
+initSlick($('.slider-nav'), propsNav);
+
+
+var props = {
+        active: 0,
+        collapsible: false ,
+        startCollapsed: false,
+        activate: function(event, tab){
+            var nav  = $(tab.panel).find('.slider-nav'),
+                main = $(tab.panel).find('.slider-for'),
+                asMain = main.attr('id'),
+                asNav  = nav.attr('id');
+            mainSlider.asNavFor = '#' + asNav;
+            navSlider.asNavFor = '#' + asMain;
+            initSlick(main, propsFor);
+            initSlick(nav, propsNav);
+        },
+        deactivate: function(event, tab){
+            var nav  = $(tab.panel).find('.slider-nav'),
+                main = $(tab.panel).find('.slider-for');
+            main.slick('unslick');
+            nav.slick('unslick');
+        }
+    }; 
+    var type = {
+        active: 0,
+        collapsible: false,
+        startCollapsed: false,
+        activate: function(event, tab){
+            $(tab.panel).siblings('ul').find('select').val(tab.id);
+            var id = $(tab.panel).find('.tabs__content').attr('id');
+            setTimeout(function(){
+                try {
+                    $('#' + id).responsiveTabs('activate', 0);
+                } catch (err) {}
+            }, 100);
+       }
+    };
+initTabs($('.tabs__main'), type);
+initTabs($('.tabs__main'), props);
 
 
 
 
 
 
-   function initTabs(tab, props) {
-        $.ionTabs(tab, props);
+ function initTabs(item, props) {
+     item.responsiveTabs(props);
    }
+
+
+function initSlick(slider, props) {
+  slider.slick(props);
+}
 
 });
